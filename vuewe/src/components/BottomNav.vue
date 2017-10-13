471c09a706db835bfcd4fedecac4cd4771829437
zhushuai <1607291079@qq.com>
2017-10-13 14:37:04 +0800

    for weui

---
+++ vuewe/src/components/BottomNav.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="weui-tabbar">
+        <a href="javascript:;" class="weui-tabbar__item" :class="index == 0 ? 'weui-bar__item_on' : ''" @click="changeIndex(0)">
+        	<i class="iconfont big">&#xe8b9;</i>
+            <p class="weui-tabbar__label">主页</p>
+        </a>
+        <a href="javascript:;" class="weui-tabbar__item" :class="index == 1 ? 'weui-bar__item_on' : ''" @click="changeIndex(1)">
+        	<i class="iconfont big">&#xe630;</i>
+            <p class="weui-tabbar__label">阅读</p>
+        </a>
+        <a href="javascript:;" class="weui-tabbar__item" :class="index == 2 ? 'weui-bar__item_on' : ''" @click="changeIndex(2)">
+        	<i class="iconfont big">&#xe8cc;</i>
+            <p class="weui-tabbar__label">写作</p>
+        </a>
+        <a href="javascript:;" class="weui-tabbar__item" :class="index == 3 ? 'weui-bar__item_on' : ''" @click="changeIndex(3)">
+        	<i class="iconfont big">&#xe8c8;</i>
+            <p class="weui-tabbar__label">个人</p>
+        </a>
+    </div>
+</template>
+<script>
+export default {
+	name: 'bottomNav',
+	data: function(){
+		return {
+			index: 0
+		}
+	},
+	methods: {
+		changeIndex(nummber){
+			this.index = nummber;
+			this.$emit('indexChange',nummber);
+		}
+	}
+}
+</script>
---
+++ vuewe/src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="weui-tab">
+	    <div class="weui-tab__panel">
+	    </div>
+	    <my-nav @indexChange="indexed"></my-nav>   
+	</div>
+</template>
+<script>
+import bottomNav from "@/components/BottomNav"
+export default {
+	name: 'main',
+	components: {
+	    'my-nav': bottomNav
+	},
+	methods: {
+		indexed: function(nummber) {
+			console.log(nummber);
+		}
+	}
+}
+</script>
